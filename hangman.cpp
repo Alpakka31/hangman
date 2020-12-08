d34e8a486cd5c2d6a5e420aadb9f20b5e40b82e6
Alpakka31 <patukka31@gmail.com>
2020-12-08 12:31:59 +0200

    clean up code

--- hangman.cpp
+++ hangman.cpp
@@ -13,9 +13,9 @@ LICENSE file in the root directory of this source tree.
 
 // Clear terminal screen
 void clearScreen() {
-#if defined _WIN32 // If using Windows use cls
+#if defined _WIN32
     system("cls");
-#elif defined __linux__ // If using Linux use clear
+#elif defined __linux__
     system("clear");
 #endif
 }
@@ -27,32 +27,25 @@ class Hangman {
         std::string incompleteWord; // The incomplete word
         std::vector<std::string> wordlist; // List of words
 
-        int lives; // Lives left
+        int lives;
     public:
-        // Constructor
         Hangman() {
-            // Construct the wordlist
             constructWordlist();
-
-            // Initialize game
             initializeGame();
         }
 
         // A function that constructs the wordlist
         void constructWordlist() {
-            // Read words from a file
             std::ifstream file("wordlist.txt");
 
-            // If file opened succesfully do this
             if (file.is_open()) {
                 std::string line;
 
-                // Read file line by line
                 while(std::getline(file, line)) {
-                    wordlist.push_back(line); // Add the word to the wordlist
+                    wordlist.push_back(line);
                 }
-                file.close(); // Close the file
-            } else { // Otherwise on error exit the game
+                file.close();
+            } else {
                 std::cout << "Couldn't open file!\n";
                 exit(1);
             }
@@ -60,23 +53,21 @@ class Hangman {
 
         // A function that initializes the game
         void initializeGame() {
-            // Create the RNG
             std::random_device dev;
             std::mt19937 rng(dev());
 
-            // Generate random number between 0 and 25
             int amountOfWords = wordlist.size() - 1;
             std::uniform_int_distribution<std::mt19937::result_type> dist(0, amountOfWords);
-            word = wordlist.at(dist(rng)); // Use a random word from the wordlist as the correct word
+            word = wordlist.at(dist(rng));
 
-            incompleteWord.clear(); // If re-initializing the game, clear the incomplete word
+            incompleteWord.clear();
 
             // Add '_' characters to the incomplete word (resembles hidden word)
             for (size_t i = 0; i < word.length(); i++) {
                 incompleteWord += "_";
             }
 
-            lives = 6; // Player has 6 lives
+            lives = 6;
         }
 
         // A function that shows the amount of lives left
@@ -92,7 +83,7 @@ class Hangman {
         // Show the correct hanging man
         void showMan() {
             switch (lives) {
-                case 6: // 6 lives left
+                case 6:
                     std::cout << " ------\n";
                     std::cout << " |    |\n";
                     std::cout << "      |\n";
@@ -101,25 +92,25 @@ class Hangman {
                     std::cout << "      |\n";
                     std::cout << "--------\n";
                     break;
-                case 5: // 5 lives left
-                    std::cout << " ------\n";
-                    std::cout << " |    |\n";
-                    std::cout << " O    |\n";
-                    std::cout << "      |\n";
-                    std::cout << "      |\n";
-                    std::cout << "      |\n";
-                    std::cout << "--------\n";
-                    break;
-                case 4: // 4 lives left
+                case 5:
                     std::cout << " ------\n";
                     std::cout << " |    |\n";
                     std::cout << " O    |\n";
+                    std::cout << "      |\n";
+                    std::cout << "      |\n";
+                    std::cout << "      |\n";
+                    std::cout << "--------\n";
+                    break;
+                case 4:
+                    std::cout << " ------\n";
+                    std::cout << " |    |\n";
+                    std::cout << " O    |\n";
                     std::cout << " |    |\n";
                     std::cout << "      |\n";
                     std::cout << "      |\n";
                     std::cout << "--------\n";
                     break;
-                case 3: // 3 lives left
+                case 3:
                     std::cout << " ------\n";
                     std::cout << " |    |\n";
                     std::cout << "\\O    |\n";
@@ -128,7 +119,7 @@ class Hangman {
                     std::cout << "      |\n";
                     std::cout << "--------\n";
                     break;
-                case 2: // 2 lives left
+                case 2:
                     std::cout << " ------\n";
                     std::cout << " |    |\n";
                     std::cout << "\\O/   |\n";
@@ -137,7 +128,7 @@ class Hangman {
                     std::cout << "      |\n";
                     std::cout << "--------\n";
                     break;
-                case 1: // 1 lives left
+                case 1:
                     std::cout << " ------\n";
                     std::cout << " |    |\n";
                     std::cout << "\\O/   |\n";
@@ -146,7 +137,7 @@ class Hangman {
                     std::cout << "      |\n";
                     std::cout << "--------\n";
                     break;
-                case 0: // No lives left
+                case 0:
                     std::cout << " ------\n";
                     std::cout << " |    |\n";
                     std::cout << "\\O/   |\n";
@@ -168,40 +159,34 @@ class Hangman {
 
         // A function that validates the input the player gave
         void validateAnswer(std::string answer) {
-            // If the character is found in the correct word, then modify the incomplete word.
-            // (correct answer)
+            // correct answer
             if (word.find(answer[0]) != std::string::npos) {
                 for (size_t i = 0; i < word.length(); i++) {
-                    // Set the correct character to the correct position in the incomplete word
                     if (word[i] == answer[0]) {
                         incompleteWord[i] = answer[0];
                     }
                 }
             } else {
-                // If the character wasn't found in the correct word, then the player loses a life
-                // (incorrect answer)
+                // incorrect answer
                 lives--;
             }
         }
 
         // The main game loop
         void run() {
-            clearScreen(); // Clear terminal screen
+            clearScreen();
             std::cout << "A very simple hangman game.\n";
 
-            // Game runs in a infinite while loop
             while (true) {
-                showLives(); // Show lives left
-                showWord(); // Show the incomplete word to the player
-                showMan(); // Show the hanging man
+                showLives();
+                showWord();
+                showMan();
 
-                // Ask input from the player and validate it
                 std::cout << "Give a character: ";
                 std::string input = getInput();
                 validateAnswer(input);
                 std::cout << "\n";
 
-                // If the player has no lives left, then the player loses the game
                 if (lives == 0) {
                     showLives();
                     showWord();
@@ -210,7 +195,6 @@ class Hangman {
                     std::cout << "You lost!\n";
                     std::cout << "The correct word was: " << word << "\n";
                     break;
-                // If the player completes the incomplete word, then the player wins
                 } else if (incompleteWord == word) {
                     showLives();
                     showWord();
@@ -221,17 +205,16 @@ class Hangman {
                 }
             }
 
-            // Ask if the player wants to play again
             std::cout << "\n\nDo you want to play again? (yes/no)\n";
             std::string input = getInput();
 
             if (input == "yes") {
-                initializeGame(); // Re-initialize the game
-                run(); // Run the game
+                initializeGame();
+                run();
             } else if (input == "no") {
-                ;; // Do nothing
+                ;;
             } else {
-                ;; // Do nothing
+                ;;
             }
         }
 };
